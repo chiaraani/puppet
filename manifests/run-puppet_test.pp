607f5714f879980ee1118fc9ee72776cd0004a24
Chiara Ezkurra <chiaraani@gmail.com>
2018-03-16 16:59:28 +0000

    copying files

--- manifests/run-puppet_test.pp
+++ manifests/run-puppet_test.pp
@@ -2,3 +2,7 @@ file { '/tmp/hello.txt':
   ensure => file,
   content => 'hello, how are you?'
 }
+file { '/tmp/hola.txt':
+  ensure => present,
+  source => '/etc/puppetlabs/code/environments/production/files/hola.txt'
+}
